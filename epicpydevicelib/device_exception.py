a729ece33e86f3d56918445834bffe433ff59a44
nogard <nogard@ucsc.edu>
2025-10-26 14:40:22 -0700

    adding flag to determine if epic raises on device exception.

--- epicpydevicelib/device_exception.py
+++ epicpydevicelib/device_exception.py
@@ -1,12 +1,12 @@
 from epiclibcpp.epiclib import raise_device_exception
 
-# raise_device_exception = raise_device_exception
-
+raise_on_device_error: bool = False
 
 class Device_exception(Exception):
     def __init__(self, message):
         super(Device_exception, self).__init__(message)
-        raise_device_exception(message)  # TODO: WHY??
+        if raise_on_device_error:
+            raise_device_exception(message)
 
 
 if __name__ == "__main__":
